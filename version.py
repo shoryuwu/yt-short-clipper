b3d3f698ec1f615fa36a71c6360b939e0923e633
jipraks <ajiprakoso1995@gmail.com>
2026-02-01 09:20:07 +0700

    Increase Version

--- version.py
+++ version.py
@@ -2,8 +2,8 @@
 YT-Short-Clipper Version Information
 """
 
-__version__ = "0.0.12"
-__version_info__ = (0, 0, 12)
+__version__ = "0.0.13"
+__version_info__ = (0, 0, 13)
 
 # Update URL
 UPDATE_CHECK_URL = "https://api.ytclip.org/webhook/yt-clipper/latest-version"
